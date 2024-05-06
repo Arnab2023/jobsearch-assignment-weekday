9671cb77dc27f289fe4f4a1aad60d1b5a599f2b9
Arnab2023 <arnab192023@gmail.com>
2024-05-06 20:45:10 +0530

    Created FilterFunction page and added add, remove and reset filter logic

---
+++ src/util/FilterFunction.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+
+const useFilter = (initialData) => {
+  const [data, setData] = useState(initialData);
+  const [filters, setFilters] = useState([]);
+
+  const applyFilters = () => {
+    let filteredData = [...initialData];
+    if (filters.length > 0) {
+      filteredData = filteredData.filter((item) =>
+        filters.every((filter) => {
+          const { operand, opcode } = filter;
+          return opcode(operand, item);
+        })
+      );
+    }
+    setData(filteredData);
+  };
+
+  const addFilter = (filterFunction) => {
+    setFilters((prevFilters) => [...prevFilters, filterFunction]);
+  };
+
+  const removeFilter = (filterFunction) => {
+    setFilters((prevFilters) =>
+      prevFilters.filter((filter) => filter !== filterFunction)
+    );
+  };
+
+  const resetFilters = () => {
+    setFilters([]);
+    setData(initialData);
+  };
+
+  useEffect(() => {
+    console.log(filters);
+    applyFilters();
+  }, [filters, initialData]);
+
+  return { data, addFilter, removeFilter, resetFilters };
+};
+
+export default useFilter;
